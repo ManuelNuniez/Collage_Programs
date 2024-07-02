3ba4c8019cd1ee1ad2e514b0fbc4540232acd552
ManuelNuniez <163766190+ManuelNuniez@users.noreply.github.com>
2024-07-02 18:06:58 +0000

    agregue el metodo "CopiarConjunto" para corregir la impresión del mismo, porque al imprimirlo se borraba el original

--- src/algoritmos/MetodosConjunto.java
+++ src/algoritmos/MetodosConjunto.java
@@ -6,15 +6,41 @@ import impl.ConjuntoDinamico;
 public class MetodosConjunto {
     
     public static void ImprimirConjunto(ConjuntoTDA original){
-        ConjuntoTDA c = new ConjuntoDinamico();
-        c.InicializarConjunto();
-        c=original;
-        while (!c.ConjuntoVacio()) {
-            int x=c.Elegir();
-            System.out.print(" " + x);
-            c.Sacar(x);
+
+        if (original.ConjuntoVacio()) {
+            System.out.println("conjunto vacio");
+        }else{
+            ConjuntoTDA c=CopiarConjunto(original);
+            while (!c.ConjuntoVacio()) {
+                int x=c.Elegir();
+                System.out.print(" " + x);
+                c.Sacar(x);
+            }
         }
-        
-        
+    }
+
+    public static ConjuntoTDA CopiarConjunto(ConjuntoTDA original){
+        ConjuntoTDA copia = new ConjuntoDinamico();
+        copia.InicializarConjunto();
+
+        ConjuntoTDA aux = new ConjuntoDinamico();
+        aux.InicializarConjunto();
+
+        while (!original.ConjuntoVacio()) {
+            int x = original.Elegir();
+            original.Sacar(x);
+            
+            aux.Agregar(x);
+            copia.Agregar(x);
+        }
+        while (!aux.ConjuntoVacio()) {
+            int x = aux.Elegir();
+            aux.Sacar(x);
+
+            original.Agregar(x);
+        }
+
+        return copia;
+
     }
 }
